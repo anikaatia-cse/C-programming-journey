961f1cb92758cb027583e464e38c6266a84ea537
anikaatia-cse <anikaatia23@gmail.com>
2026-05-24 09:48:23 +0600

    Initial commit: Added C Program Structure blueprint
    Created lesson1_structure.c to document the basic anatomy of a C program.

---
+++ 01-Introduction/lesson1_structure.c
@@ -0,0 +1,12 @@
+// 1. HEADER SECTION (Pre-processor Directives)
+#include <stdio.h> // Standard Input Output Library
+// 2. MAIN FUNCTION (The Entry Point)
+int main()
+{
+    // 3. VARIABLE DECLARATION (Creating boxes for data)
+    int gpa_score = 4;
+    // 4. ACTION STATEMENTS (Doing the actual work)
+    printf("Welcome to CSE! Your starting target is a %d.0 GPA\n", gpa_score);
+    // 5. RETURN STATEMENT (Telling the system, we're done!)
+    return 0;
+}
