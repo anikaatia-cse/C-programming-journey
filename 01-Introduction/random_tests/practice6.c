f7dd44922da631499564f7099b4c6a01f67ff6bd
anikaatia-cse <anikaatia23@gmail.com>
2026-06-29 21:17:32 +0600

    Add practice6.c to gitignore.

---
+++ 01-Introduction/random_tests/practice6.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter the value of a:");
+    scanf("%d", &a);
+    printf("Enter the value of b:");
+    scanf("%d", &b);
+
+    //likewise you can also go for subtraction, multiplication, division, etc. by using the same logic but different operators.
+    printf("The sum of the equation is : %d", a + b);
+    return 0;
+}
