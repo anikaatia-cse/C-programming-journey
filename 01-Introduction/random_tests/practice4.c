960370860a1bd72066188870d4a378ba2927803c
anikaatia-cse <anikaatia23@gmail.com>
2026-06-29 20:10:05 +0600

    Add practice4.c to gitignore

---
+++ 01-Introduction/random_tests/practice4.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main()
+{
+    int age = 19;
+    float pi = 3.14;
+    char grade = 'A';
+
+    printf("My age is: %d \n", age);
+    printf("The constant value of pi is: %f \n", pi);
+    printf("My HSC grade is: %c \n", grade);
+}
