88d8fbcad0393cb041d0d7786302c02554b3d48c
anikaatia-cse <anikaatia23@gmail.com>
2026-06-29 08:11:46 +0600

    Add practice3.c to gitignore.

---
+++ 01-Introduction/random_tests/practice3.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main()
+{
+    /*
+    Constants( const ): Values that don't change and  are fixed.
+     There are 3 types of constants
+     Integer Constants ( 1, 2, 3, 0, -1, -2, -1 )
+     Real Constants ( 1.0, 3.14, -2.4 )
+     Character Constants ( 'a', 'A', '*', '#', '&' ) 
+    */
+    return 0;
+}
