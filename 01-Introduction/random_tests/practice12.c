f42122548a08cfc02b794db918d16453b467dd97
anikaatia-cse <anikaatia23@gmail.com>
2026-07-01 07:21:23 +0600

    Add practice12.c to gitignore.

---
+++ 01-Introduction/random_tests/practice12.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+# include<math.h>
+//Question: Solve a = 1.999999; and print the value of a as an integer.
+int main()
+
+{
+    int a = (int) 1.999999;
+    printf("%d\n", a);
+    return 0;
+}
