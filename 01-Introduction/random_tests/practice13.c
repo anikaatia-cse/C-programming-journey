759eda6ad24547b857db15502f0b7f30f31e482a
anikaatia-cse <anikaatia23@gmail.com>
2026-07-02 23:09:22 +0600

    Add practice13.c to gitignore

---
+++ 01-Introduction/random_tests/practice13.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+//QUESTION: print the value for x = 4 + 9 * 10   and y = 3 * 4 / 6 * 2 - 1.
+/* Operator Precedence Priority List:
+1. Parentheses ()
+2. Multiplication *, Division /, Modulus %
+3. Addition +, Subtraction -
+4. Assignment =, +=, -=, *=, /=, %=
+5. Comparison Operators: ==, !=, >, <, >=, <=
+6. Logical Operators: &&, ||, !
+7. Bitwise Operators: &, |, ^, ~, <<, >>
+*/ 
